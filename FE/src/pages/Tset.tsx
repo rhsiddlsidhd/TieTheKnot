94e05fb98163171dd86d50b9f40abf67c37c5a10
Young <rhsiddlsidhd@nate.com>
2024-12-14 20:25:24 +0900

    Gallery 개별 상태 관리 수정 테스트

--- FE/src/pages/Tset.tsx
+++ FE/src/pages/Tset.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-
-type Count = Number;
-interface GalleryType {
-  type: string;
-}
 
 const Tset = () => {
   const [count, setCount] = useState(0);
-  const [isOpen, setIsOpen] = useState(true);
   const [galleryType, setGalleryType] = useState<string>("");
+  /**
+   * count Type을 하나의 객체로 받아서 상태관리 해야함
+   *
+   */
+
+  const [galleryId, setGalleryId] = useState<string[]>([]);
+  const [toggleDropdown, setToggleDropdown] = useState<boolean[]>([]);
+  const [selectedGalleryTypeId, setSelectedGalleryTypeId] =
+    useState<String>("");
 
   //이중배열
 
@@ -32,41 +35,16 @@ const Tset = () => {
       quantity: 4,
       type: [
         ["a", "b"],
-        ["c", "d"],
+        ["a", "c"],
+        ["d", "c"],
       ],
     },
   };
+  type ImgTypeKey = keyof typeof imgType;
 
   // A 타입은 이미지 2개
   // B 타입은 이미지 3개
   // C 타입은 이미지 4개
-
-  //   const typeConvert = (s: string[][]) => {
-
-  //     let n = "";
-
-  //     // 2차원 배열을 순차적으로 처리
-  //     s.forEach((row) => {
-  //       // 각 행의 요소들을 공백으로 구분하여 추가
-  //       n += `"${row.join(" ")}"${"\n"}`;
-  //     });
-
-  //     console.log(n); // 결과 출력
-  //     return n; // 결과 반환
-  //   };
-
-  // const typeConvert = (s: string[][]) => {
-  //     let n = "";
-
-  //     // 2차원 배열을 순차적으로 처리
-  //     s.forEach((row) => {
-  //       // 각 행의 요소들을 공백으로 구분하여 추가
-  //       n += `"${row.join(" ")}"${"\n"}`;
-  //     });
-
-  //     console.log(n); // 결과 출력
-  //     return n; // 결과 반환
-  //   };
 
   const typeConvert = (s: string[][]) => {
     let n = "";
@@ -78,16 +56,21 @@ const Tset = () => {
     return n;
   };
 
-  const handleTypeValue = (e: React.MouseEvent<HTMLElement>) => {
+  const handleTypeValue = (id: String, e: React.MouseEvent<HTMLElement>) => {
     const target = e.target as HTMLElement;
     const value = target.getAttribute("value");
 
+    setSelectedGalleryTypeId(id);
+
+    /**
+     * 클릭시 dropdown 아이디와 갤러리 아이디가 일치한 것만 데이터 전달달
+     * 여기에서 id 와 components id를 받아와야하네 ? ㅡ.ㅡ ;;
+     */
     if (value) {
       const typeObj = JSON.parse(value);
       const { quantity, type } = typeObj;
 
       setCount(quantity);
-      setIsOpen(!isOpen);
       if (type) {
         let $type = typeConvert(type);
         setGalleryType($type);
@@ -95,45 +78,101 @@ const Tset = () => {
     }
   };
 
+  function solution(a: typeof imgType): ImgTypeKey[] {
+    const result: ImgTypeKey[] = [];
+    for (const key in a) {
+      result.push(key as ImgTypeKey);
+    }
+    return result;
+  }
+
+  const galleryCountFn = () => {
+    /**
+     * + 버튼 클릭시 랜덤 ID 생성 및 idArray.push(randomId)
+     * idArray.length 만큼 drop 및 gallery 뿌려주기
+     */
+    let galleryId = generateRandomId();
+    setGalleryId((prev) => [...prev, galleryId]);
+  };
+
+  useEffect(() => {
+    //갤러리 아이디 만큼 boolean 값으로 된 배열을 추가해야함
+    if (galleryId.length !== 0) {
+      setToggleDropdown((prev) => [...prev, false]);
+    }
+  }, [galleryId]);
+
+  const imgTypeArray: ImgTypeKey[] = solution(imgType);
+
+  const generateRandomId = () => {
+    let id = "";
+    const length = 12;
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < length; i++) {
+      id += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return id;
+  };
+
+  const handleDropdown = (index: Number, e?: React.MouseEvent<HTMLElement>) => {
+    setToggleDropdown((prev) =>
+      prev.map((open, i) => (i === index ? !open : open))
+    );
+  };
+
   return (
     <div style={{ padding: "1rem" }}>
-      <Drop>
-        <div onClick={() => setIsOpen((prevState) => !prevState)}>
-          {isOpen ? "off" : "on"}
-        </div>
-        {!isOpen && (
-          <ul onClick={handleTypeValue} style={{ listStyle: "none" }}>
-            <li value={JSON.stringify(imgType.a)}>1</li>
-            <li value={JSON.stringify(imgType.b)}>2</li>
-            <li value={JSON.stringify(imgType.c)}>3</li>
-          </ul>
-        )}
-      </Drop>
-
-      <div>{count}</div>
-      <GridContainer galleryType={galleryType}>
-        {Array.from({ length: count }, (_, i) => {
-          const letter = String.fromCharCode(97 + i);
-          return (
-            <img
-              key={i}
-              className={`items item${i + 1}`}
-              src={`${process.env.REACT_APP_IMAGE_BASE_URL}/married.jpg`}
-              alt="이미지"
-              style={{ gridArea: letter }}
-            />
-          );
-        })}
-      </GridContainer>
-
-      {/* <GalleryWrapper>
-        {}
-        <img
-          className="items item1"
-          src={`${process.env.REACT_APP_IMAGE_BASE_URL}/married.jpg`}
-          alt="이미지"
-        />
-      </GalleryWrapper> */}
+      <button style={{ padding: "1rem" }} onClick={galleryCountFn}>
+        plus
+      </button>
+      <h1>갤러리 카운트 {galleryId.length}</h1>
+      {Array.from({ length: galleryId.length }, (_, i) => {
+        return (
+          <Drop className={galleryId[i]}>
+            {/* 드롭에 현재 id가 다 다르니 일치한것만 open 하고 나머지는 close */}
+            {/* <div onClick={() => setIsOpen((prevState) => !prevState)}> */}
+            <div onClick={() => handleDropdown(i)}>
+              {toggleDropdown[i] ? "on" : "off"}
+            </div>
+            {toggleDropdown[i] && (
+              <ul
+                onClick={(e) => handleTypeValue(galleryId[i], e)}
+                style={{ listStyle: "none" }}
+              >
+                {imgTypeArray.map((v, i) => {
+                  const value = JSON.stringify(imgType[v]);
+                  return (
+                    <li key={i} value={value}>
+                      {i + 1}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </Drop>
+        );
+      })}
+      <div>갤러리 items {count}</div>
+      {Array.from({ length: galleryId.length }, (_, i) => {
+        return (
+          <GridContainer galleryType={galleryType} className={galleryId[i]}>
+            {Array.from({ length: count }, (_, i) => {
+              const letter = String.fromCharCode(97 + i);
+              return (
+                <img
+                  key={i}
+                  className={`items item${i + 1}`}
+                  src={`${process.env.REACT_APP_IMAGE_BASE_URL}/married.jpg`}
+                  alt="이미지"
+                  style={{ gridArea: letter }}
+                />
+              );
+            })}
+          </GridContainer>
+        );
+      })}
     </div>
   );
 };
@@ -158,7 +197,6 @@ const GridContainer = styled.div<{ galleryType: string }>`
   grid-auto-rows: 8rem; */
   grid-auto-rows: 8rem;
   grid-template-areas: ${({ galleryType }) => galleryType};
-
   grid-gap: 1rem;
   padding: 1rem;
   & > img {
