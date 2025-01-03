3869fa0d64cdbd0755c6b7ae4dcba2769483aeb1
Young <rhsiddlsidhd@nate.com>
2025-01-03 22:02:06 +0900

    FE update

--- FE/src/pages/Main.tsx
+++ FE/src/pages/Main.tsx
@@ -18,10 +18,11 @@ import CalenderSections from "../components/calenders/CalenderSections";
 
 import SubwaySections from "../components/locations/SubwaySections";
 import BusSections from "../components/locations/BusSections";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GeoProps } from "../apis/api/location/kakaoMap/types";
 import NaviSections from "../components/locations/NaviSections";
 import PrivateCarSections from "../components/locations/PrivateCarSections";
+import axios from "axios";
 
 export interface WeddingDay {
   year: number;
--- FE/src/tests/googleLogin/Test.tsx
+++ FE/src/tests/googleLogin/Test.tsx
@@ -1,24 +1,23 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 import styled from "styled-components";
 
 const Test = () => {
-  const googleLogin = () => {
-    const clientId = `362838615937-hjspjrai3sovuqu9pbqpsju3onpjgb0t.apps.googleusercontent.com`;
-    const redirectUri = `http://localhost:3000/test`;
-    const responseType = "token";
-    const scope = `https://www.googleapis.com/auth/userinfo.email`;
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const state = query.get("state");
+  const code = query.get("code");
 
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope}&response_type=${responseType}&redirect_uri=${redirectUri}&client_id=${clientId}&include_granted_scopes=true`;
-
-    window.location.href = authUrl;
+  const googleLoginLocation = async () => {
+    window.location.href = "http://localhost:8080/auth";
   };
 
   return (
     <Container>
       <p>Google 로그인</p>
       <div>
-        <button onClick={googleLogin}>로그인 버튼</button>
+        <button onClick={googleLoginLocation}>구글버튼</button>
       </div>
     </Container>
   );
