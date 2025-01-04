f9e8ae881401738cd90c80e1c2b790c3eec9abba
Young <rhsiddlsidhd@nate.com>
2025-01-04 14:35:21 +0900

    token management

--- FE/src/pages/Register.tsx
+++ FE/src/pages/Register.tsx
@@ -1,14 +1,30 @@
-import React, { MouseEventHandler, useContext } from "react";
-import { TypeOfSelected } from "../App";
+import axios from "axios";
+import { useEffect } from "react";
 import styled from "styled-components";
-import { Navigate, useNavigate } from "react-router";
 
 const Register = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("http://localhost:8080/auth/token");
+        const token = res.data.accessToken;
+        setAccessTokenToStorage(token);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  const setAccessTokenToStorage = (token: string): void => {
+    console.log(token);
+  };
+
   return (
-    <>
+    <div>
       <button style={{ padding: "1rem" }}>plus</button>
       <Drop>등록</Drop>
-    </>
+    </div>
   );
 };
 
